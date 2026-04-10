4196c760585e78c61a3bef313716aee818134d3a
Utshav-Raj <utsavraj1235@gmail.com>
2026-04-11 02:46:15 +0530

    Refactor model imports and exports in __init__.py
    Consolidate imports and update __all__ exports.

--- models/__init__.py
+++ models/__init__.py
@@ -1,39 +1,6 @@
-"""Typed models exported by the SmartOps AI package."""
-
-from .openenv import SmartOpsAction, SmartOpsObservation, SupportState
-from .reward import RewardComponents, TaskGrade, TicketReward
-from .ticket import (
-    ActionRecord,
-    ActionType,
-    MetricsSnapshot,
-    QueueSummary,
-    SupportTicket,
-    TaskDifficulty,
-    TaskScenario,
-    TicketCategory,
-    TicketPublicView,
-    TicketSentiment,
-    TicketStatus,
-    TicketUrgency,
+from .openenv import (
+    ActionRecord, ActionType, MetricsSnapshot, QueueSummary,
+    SmartOpsAction, SmartOpsObservation, SupportState, SupportTicket,
+    TaskDifficulty, TaskGrade, TaskScenario, TicketCategory,
+    TicketPublicView, TicketReward, TicketSentiment, TicketStatus, TicketUrgency,
 )
-
-__all__ = [
-    "ActionRecord",
-    "ActionType",
-    "MetricsSnapshot",
-    "QueueSummary",
-    "RewardComponents",
-    "SmartOpsAction",
-    "SmartOpsObservation",
-    "SupportState",
-    "SupportTicket",
-    "TaskDifficulty",
-    "TaskGrade",
-    "TaskScenario",
-    "TicketCategory",
-    "TicketPublicView",
-    "TicketReward",
-    "TicketSentiment",
-    "TicketStatus",
-    "TicketUrgency",
-]
